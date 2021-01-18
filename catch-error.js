9dda6d9b850f73dc3b92cd7c8db9850acdd8ea4e
Andrey1303110 <andrey1303110@gmail.com>
2021-01-18 20:01:39 +0200

    commit next

--- catch-error.js
+++ catch-error.js
@@ -1,16 +1,36 @@
-let json = `{ "age": 34, "name": "Dmytro" }`;
+let json = `{ "age": 34, "name: "Dmytro" }`;
 
-try {
-  //alert("Hello World!!!');
-  //eval(`alert("Hello World!!!')`);
+let fn = () => {
+  try {
+    //alert("Hello World!!!');
+    //eval(`alert("Hello World!!!')`);
 
-  let data = JSON.parse(json);
-  let name = data.name;
-  console.log(name);
+    let data = JSON.parse(json);
+    let name = data.name;
+    console.log(name);
 
-} catch (error) {
-  console.log(error.name);
-  console.log(error.message);
+  } catch (error) {
+    //console.log(error);
+    //console.log(error.name);
+    //console.log(error.message);
+
+    switch (error.name) {
+      case ("SyntaxError"):
+        console.log(`Синтаксическая ошибка: ${error.message}`);
+
+        break;
+    }
+
+    if (error instanceof SyntaxError) {
+      console.log(`Синтаксическая ошибка: ${error.message}`);
+    }
+
+    return;
+  } finally {
+    console.log("JSON was processed!");
+  }
+
+  //console.log("JSON was processed!");
 }
 
-console.log("Finished");
+fn();
